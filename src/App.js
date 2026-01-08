45b1abbf6d632e00b851469ac9915f3292ca2a9c
danicabeau <126159395+danicabeau@users.noreply.github.com>
2026-01-08 10:25:17 +0700

    problem with event handle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import { Footer, Header,Content } from './func-components';
 import React from 'react';
-import Calendar from './class-components';
+
 import logo from './logo.svg';
+import Button from './class-components';
 //import './style.css';
 
 function App() {
-  return <>
-    <Header/>
-    <p><center><Calendar/></center></p>
-    <Content/>
-    <Footer/>
-  </>
+  return <Button/>
     
 
   
