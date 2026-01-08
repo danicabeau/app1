3263b78b065935a5469e6f63357cf204aea517f6
danicabeau <126159395+danicabeau@users.noreply.github.com>
2026-01-08 11:44:51 +0700

    Event Validation

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import React from 'react';
 import logo from './logo.svg';
 import Button from './class-components';
 import { Calculator2 } from './calculator';
+import { EventData1 } from './event-data';
 //import './style.css';
 export default function App(){
-  return<Calculator2/>
+  return<EventData1/>
 }
+//export default function App(){
+//  return<Calculator2/>
+//}
 //function App() {
 //  return <Button/>
 //    
