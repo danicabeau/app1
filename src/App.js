b8776e22723e23b40d14a266452dc1d9ccfd4727
danicabeau <126159395+danicabeau@users.noreply.github.com>
2026-01-15 10:14:30 +0700

    add RefsArray

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import logo from './logo.svg';
 import Button from './class-components';
 import { Calculator2 } from './calculator';
 import { EventData1 } from './event-data';
+import RefsArray from './refs-array';
 //import './style.css';
 export default function App(){
-  return<EventData1/>
+  return <RefsArray/>
 }
+//export default function App(){
+//  return<EventData1/>
+//}
 //export default function App(){
 //  return<Calculator2/>
 //}
