e9933e54de10be4fdd5301a1ad19d6a9703d2e57
danicabeau <126159395+danicabeau@users.noreply.github.com>
2026-02-12 10:03:13 +0700

    add Route

--- src/App.js
+++ src/App.js
@@ -1,43 +1,6 @@
-import { Footer, Header,Content } from './func-components';
-import React from 'react';
-import Content2 from './context-content2';
-import Header2 from './context-header2';
-import {userContext} from './context';
+import React from 'react'; // เติม React
+import Router2 from "./router2"; // เติมชื่อ Component Router2
 
-import logo from './logo.svg';
-import Button from './class-components';
-import { Calculator2 } from './calculator';
-import { EventData1 } from './event-data';
-import RefsArray from './refs-array';
-import MessageBox from './state-func';
-export default function App(){
-  const [user, setUser] = React.useState('');
-
-
-  return (
-    <userContext.Provider value={{user,setUser}}>
-    <Header2/>
-    <Content2/>
-    </userContext.Provider>
-  )
+export default function App() { // ตั้งชื่อฟังก์ชัน App
+    return <Router2 />;
 }
-//import './style.css';
-//export default function App(){
-//  return <RefsArray/>
-//}
-//export default function App(){
-//  return<EventData1/>
-//}
-//export default function App(){
-//  return<Calculator2/>
-//}
-//function App() {
-//  return <Button/>
-//    
-//
-//  
-    
-//    
-//}
-
-//export default App;
